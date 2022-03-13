be9ed1efe509768cb8c30f7bccd0dd3686c562c1
Maksym Stepanenko <maksym.stepanenko@opower.com>
2022-03-13 14:46:21 +0200

    Observer reimplemented using COWAraySet instead of Vector.

--- src/eu/javaspecialists/courses/datastructures/ch4_sets/_4_3_ConcurrentSkipListSet.java
+++ src/eu/javaspecialists/courses/datastructures/ch4_sets/_4_3_ConcurrentSkipListSet.java
@@ -1,10 +1,20 @@
 package eu.javaspecialists.courses.datastructures.ch4_sets;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.stream.IntStream;
+
 /**
  * Thread-safe sorted set
  */
 public class _4_3_ConcurrentSkipListSet {
-  public static void main(String... args) {
 
-  }
+    public static void main(String... args) {
+        Set<Integer> data = new ConcurrentSkipListSet<>();
+        IntStream.range(0, 10_000).
+            parallel().
+            forEach(data::add);
+
+        System.out.println(data.size());
+    }
 }
--- src/eu/javaspecialists/courses/datastructures/ch4_sets/_4_4_CopyOnWriteArraySet.java
+++ src/eu/javaspecialists/courses/datastructures/ch4_sets/_4_4_CopyOnWriteArraySet.java
@@ -1,12 +1,59 @@
 package eu.javaspecialists.courses.datastructures.ch4_sets;
 
+import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * For very small sets
  */
 public class _4_4_CopyOnWriteArraySet {
-  public static void main(String... args) {
-    new CopyOnWriteArraySet();
-  }
+
+    public static void main(String... args) {
+        MyObservable model = new MyObservable();
+
+        model.addObserver(new MyObserver1());
+        model.addObserver(new MyObserver1());
+
+        model.notifyObservers("change-123");
+    }
+
+    static class MyObserver1 implements MyObserver {
+        @Override
+        public void update(MyObservable obsModel, Object arg) {
+            System.out.println("Observer2: " + arg);
+        }
+    }
+
+    @FunctionalInterface
+    interface MyObserver {
+        void update(MyObservable obsModel, Object arg);
+    }
+
+    static class MyObservable {
+
+        private final Set<MyObserver> obs = new CopyOnWriteArraySet<>();
+
+        public void addObserver(MyObserver o) {
+            if (o == null) {
+                throw new NullPointerException();
+            }
+            obs.add(o);
+        }
+
+        public void notifyObservers(Object arg) {
+            for (MyObserver singleObserver : obs) {
+                singleObserver.update(this, arg);
+            }
+        }
+
+        public void deleteObservers() {
+            obs.clear();
+        }
+
+    }
 }
+
+
+
+
+
